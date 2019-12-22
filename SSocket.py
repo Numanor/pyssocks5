56bcb00d899e7e2d9baea254b7898355ad1a7a69
Numanor <muphoonlee@gmail.com>
2019-12-22 18:35:25 +0800

    add: SHA-based data verification

--- SSocket.py
+++ SSocket.py
@@ -2,6 +2,7 @@
 import gevent
 import struct
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA
 from Crypto.Random import get_random_bytes
 
 class SSocket(gevent.socket.socket):
@@ -12,13 +13,13 @@ class SSocket(gevent.socket.socket):
             gevent.socket.socket.__init__(self)
             self.connect(addr)
         else:
-            raise Exception("XSocket.init: bad arguments")
+            raise Exception("SSocket.init: bad arguments")
         self.aes_recvbuf = ''
 
     def unpack(self, fmt, length):
         data = self.recv(length)
         if len(data) < length:
-            raise Exception("XSocket.unpack: bad formatted stream")
+            raise Exception("SSocket.unpack: bad formatted stream")
         return struct.unpack(fmt, data)
 
     def pack(self, fmt, *args):
@@ -31,26 +32,38 @@ class SSocket(gevent.socket.socket):
     
     def aes_send(self, data):
         data_len = len(data)
-        # 1. data padding
-        pad_len = data_len % AES.block_size
+        # 1. pin SHA digest
+        digest = SHA.new(data).digest()
+        data_d = data + digest
+        data_d_len = data_len + SHA.digest_size
+        # 2. data padding
+        pad_len = data_d_len % AES.block_size
         pad_len = AES.block_size - pad_len if pad_len else 0
-        data_padded = data + get_random_bytes(pad_len)
-        enc_len = data_len + pad_len
-        # 2. encrypt data
-        enc_data = self.aes.encrypt(data_padded)
-        self.pack('HH', enc_len, data_len)
-        # 3. send ciphertext
-        self.sendall(enc_data)
-        # print "data sent"
+        plaintext = data_d + get_random_bytes(pad_len)
+        total_len = data_d_len + pad_len
+        # 3. encrypt data
+        ciphertext = self.aes.encrypt(plaintext)
+        # 4.1 send header
+        self.pack('HH', total_len, data_len)
+        # 4.2 send ciphertext
+        self.sendall(ciphertext)
 
     def aes_recv(self):
+        # 1. parse header
         header = self.recv(4)
         if not header:
             return None
-        enc_len, data_len = struct.unpack('HH', header)
-        enc_data = self.recv(enc_len)
-        dec_data = self.aes.decrypt(enc_data)
-        data = dec_data[:data_len]
+        total_len, data_len = struct.unpack('HH', header)
+        # 2. read & decrypt ciphertext
+        ciphertext = self.recv(total_len)
+        plaintext = self.aes.decrypt(ciphertext)
+        data_d = plaintext[:data_len + SHA.digest_size]
+        # 3. SHA verification
+        data = data_d[:data_len]
+        recv_digest = data_d[-SHA.digest_size:]
+        comp_digest = SHA.new(data).digest()
+        if recv_digest != comp_digest:
+            raise Exception("SSocket.aes_recv: SHA digest mismatch")
         return data
     
     def aes_pack(self, fmt, *args):
